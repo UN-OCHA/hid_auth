a5751d9e2f149ec41e5dbfcf68d66a240d19ec46
Joe Turgeon <jturgeon@phase2technology.com>
2014-10-21 16:29:54 -0500

    UNCID-99: Fixing tracking redirects through login process to client site.

--- routes/session.js
+++ routes/session.js
@@ -6,7 +6,7 @@ module.exports.create = function(req, res, next) {
 
     if (user) {
       req.session.userId = user.email;
-      var redirect = (req.body.redirect != null && req.body.redirect != undefined) ? req.body.redirect : '/account';
+      var redirect = req.body.redirect || '/account';
       redirect += "?client_id=" + req.body.client_id;
       redirect += "&redirect_uri=" + req.body.redirect_uri;
       console.log('Authentication successful for ' + req.body.email + '. Redirecting to: ' + redirect);
@@ -14,7 +14,7 @@ module.exports.create = function(req, res, next) {
     }
     else {
       console.log('Authentication failed for ' + req.body.email);
-      res.status(401).render('index', {client_id: req.query.client_id, message: 'Authentication failed', csrf: req.csrfToken()});
+      res.status(401).render('index', {message: 'Authentication failed', client_id: req.body.client_id || '', redirect: req.body.redirect || '', redirect_uri: req.body.redirect_uri || '', csrf: req.csrfToken()});
     }
   });
 };
