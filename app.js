8c3824e338a5352dd0fc87dbd7e0ab7a0a3ba25f
Joe Turgeon <jturgeon@phase2technology.com>
2014-11-16 19:48:04 -0600

    Adding api/register route to allow API requests to register users. Adding routine to check API key and secret to validate access for API requests.

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.methodOverride());
 var formCSRF = function (req, res, next) {
   // Skip CSRF validation on the /oauth/access_token callback, as it's not based
   // on a form submission.
-  if (req.path == '/oauth/access_token') {
+  if (req.path == '/oauth/access_token' || req.path.substr(0, 5) == '/api/') {
     next();
   } else {
     csrf(req, res, next);
@@ -118,5 +118,7 @@ app.all('/logout', function (req, res) {
   var redirect = (req.query.redirect && String(req.query.redirect).length) ? req.query.redirect : '/';
   res.redirect(redirect);
 });
+
+app.post('/api/register', middleware.requiresKeySecret, routes.api.register);
 
 module.exports = app;
--- middleware/index.js
+++ middleware/index.js
@@ -1,5 +1,7 @@
+var async = require('async');
 var models = require('./../models');
 var User = models.User;
+var Client = models.OAuthClientsModel;
 
 function requiresUser(req, res, next) {
   if (req.session.userId) {
@@ -9,6 +11,87 @@ function requiresUser(req, res, next) {
   else {
     res.app.oauth.authorise()(req, res, next);
   }
+}
+
+function requiresKeySecret(req, res, next) {
+  var client_id,
+    access_key,
+    data,
+    clientData;
+
+  if (req.body.client_key) {
+    client_id = req.body.client_key;
+    access_key = req.body.access_key;
+    data = req.body;
+  }
+  else if (req.query.client_key) {
+    client_id = req.query.client_key;
+    access_key = req.query.access_key;
+    data = req.query;
+  }
+
+  async.series([
+    function (cb) {
+      if (!client_id || !client_id.length || !access_key || !access_key.length || !data) {
+        console.log('API request submitted without client ID, access key, or data.');
+        return cb(true);
+      }
+
+      // Step 1: Validate that the client app is allowed
+      Client.findOne({clientId: client_id}, function (err, data) {
+        if (err) {
+          console.log('Error occurred while looking up client ' + client_id);
+          return cb(err);
+        }
+        else if (data && data.clientSecret) {
+          clientData = data;
+          return cb();
+        }
+        else {
+          // Client not found.
+          console.log('API key validation failed. Client ' + client_id + ' not found.');
+          return cb(true);
+        }
+      });
+    },
+    function (cb) {
+      // Step 2: Reproduce the access key to validate the secret
+      var SHA256 = require("crypto-js/sha256");
+
+      delete data.client_key;
+      delete data.access_key;
+
+      var valuesList = flattenValues(data, '') + clientData.clientSecret;
+      var re_access_key = SHA256(valuesList);
+
+      if (access_key === re_access_key.toString()) {
+        console.log('API key/secret validated.');
+        req.client_key = client_id;
+        return cb();
+      }
+      else {
+        console.log('API key/secret validation failed.');
+        return cb(true);
+      }
+    }
+  ], function (err) {
+    return next(err);
+  });
+}
+
+function flattenValues(q, strlist) {
+  var tempList = '';
+  for (var key in q) {
+    var type = typeof q[key];
+    if (type == 'object' || type == 'array') {
+      tempList += flattenValues(q[key], tempList);
+    }
+    else {
+      tempList += q[key];
+    }
+  }
+
+  return tempList;
 }
 
 function loadUser(req, res, next) {
@@ -36,6 +119,7 @@ function notFoundHandler(req, res, next) {
 }
 
 module.exports.requiresUser = requiresUser;
+module.exports.requiresKeySecret = requiresKeySecret;
 module.exports.loadUser = loadUser;
 module.exports.isValidationError = isValidationError;
 module.exports.notFoundHandler = notFoundHandler;
