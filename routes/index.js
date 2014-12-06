d1d6ba5f236b459d779a74e71c42a3af305488be
Joe Turgeon <jturgeon@phase2technology.com>
2014-12-06 14:29:06 -0600

    Ensuring the scope parameter is passed through via login or authorization routes.

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ exports.index = function(req, res) {
       redirect_uri: req.query.redirect_uri || '',
       response_type: req.query.response_type || '',
       state: req.query.state || '',
+      scope: req.query.scope || '',
       csrf: req.csrfToken()
     });
   }
