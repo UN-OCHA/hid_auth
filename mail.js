66fa9673751823f265e14052f0a4327603ba63a3
Joe Turgeon <jturgeon@phase2technology.com>
2014-12-18 12:42:10 -0600

    Adding config setting for the SMTP user and password, and switching envs to use the humanitarianid SendGrid account.

--- mail.js
+++ mail.js
@@ -4,7 +4,11 @@ var config = require('./config');
 // create reusable transporter object using SMTP transport
 var transporter = nodemailer.createTransport({
   host: config.smtpHost || 'localhost',
-  port: config.smtpPort || 25
+  port: config.smtpPort || 25,
+  auth: {
+    user: config.smtpUser || null,
+    pass: config.smtpPass || null
+  }
 });
 
 module.exports = transporter;
