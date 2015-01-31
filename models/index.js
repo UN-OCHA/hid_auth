9c4ac9368936ee01c7616065d349bb0f71b26b1d
Joe Turgeon <jturgeon@phase2technology.com>
2015-01-31 15:33:34 -0600

    Minor code style update.

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ mongoose.connection.on('error', function(err) {
   console.error('MongoDB error: %s', err);
 });
 
-module.exports.oauth = require('./oauth');
-module.exports.User = require('./user');
-module.exports.OAuthClientsModel = require('./oauth_client');
-module.exports.mongoose = mongoose;
+exports.oauth = require('./oauth');
+exports.User = require('./user');
+exports.OAuthClientsModel = require('./oauth_client');
+exports.mongoose = mongoose;
