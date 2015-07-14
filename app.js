1b6825fddf71323c2f7b1222d03a5f2f7932dd76
Grayside <aross@phase2technology.com>
2015-07-14 10:28:53 -0700

    UNCID-672: Initial implementation of listing, view, revoke app operations. [WIP]

--- app.js
+++ app.js
@@ -250,8 +250,10 @@ app.get('/admin/users/:id/ops/:action', middleware.requiresWebUser, middleware.r
 app.post('/admin/users/:id/ops/:action', middleware.requiresWebUser, middleware.requiresAdminAccess, routes.adminUsers.action);
 
 app.get('/admin/apps', middleware.requiresWebUser, middleware.requiresAdminAccess, routes.adminApps.list);
+app.get('/admin/apps/:id', middleware.requiresWebUser, middleware.requiresAdminAccess, routes.adminApps.view);
+app.get('/admin/apps/:id/ops/:action', middleware.requiresWebUser, middleware.requiresAdminAccess, routes.adminApps.action);
+
 /*
-app.get('/admin/apps/:id/view', middleware.requiresUser, routes.admin.viewApp);
 app.get('/admin/apps/:id/edit', middleware.requiresUser, routes.admin.editApp);
 app.get('/admin/apps/:id/delete', middleware.requiresUser, routes.admin.deleteApp);
 */
